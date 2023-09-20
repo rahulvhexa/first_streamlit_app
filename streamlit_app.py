2c58515c96038d7b27709c639878e9201ca02cb7
Rahul Vashistha <137259499+rahulvhexa@users.noreply.github.com>
2023-09-20 16:27:30 +0530

    Update streamlit_app.py
    added function for recursion

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,6 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -25,6 +24,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#creating function
+def get_fruityvice_data(this_fruit_choide):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choide)
+# Take JSON data and normalize it
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Output the data as a table
+  return fruityvice_normalized
+
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -32,11 +39,8 @@ try:
   if not fruit_choice:
     streamlit.error('Please select a fruit to get information.')
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    # Take JSON data and normalize it
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # Output the data as a table
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 
 except URLError as e:
   streamlit.error()
